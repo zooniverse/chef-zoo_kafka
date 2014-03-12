38683983ad08b408edd0662d4b022f926fc4c0c4
Edward Paget <ed.paget@gmail.com>
2014-03-12 15:55:44 -0500

    Add monitrc configuration start on ebs volume creation

--- Berksfile
+++ Berksfile
@@ -6,3 +6,4 @@ cookbook 'zoo-base', path: '../zoo-base'
 cookbook 'kafka', path: '../chef-zookeeper'
 cookbook 'monit', github: 'phlipper/chef-monit', tag: '1.5.2'
 cookbook 'hostname', github: 'edpaget/chef-cookbook-hostname', branch: "feature_reload-ohai-immediately"
+cookbook 'zookeeper', github: 'zooniverse/zookeeper'
--- recipes/ec2.rb
+++ recipes/ec2.rb
@@ -1,29 +1,31 @@
 #
 # Cookbook Name:: zoo-kafka
-# Recipe:: default
+# Recipe:: ec2 
 #
-# Copyright (C) 2014 The Zooniverse
-# 
+# Copyright 2014, Zooniverse
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
-#    http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-include_recipe 'zoo-base'
-include_recipe 'kafka'
+
+include_recipe "aws"
 
 node['kafka']['number_of_brokers'].times do |n|
-  monit_monitrc "kafka-#{n}" do
-    variables({
-      pid_file_dir: node['kafka']['install_dir'],
-      n: n
-    })
+  node.default['kafka']['devices'] = '/dev/xvdf'
+
+  aws_ebs_volume "zoo_data" do
+    size 80
+    device node['kafka']['data_device']
+    volume_type "io1"
+    piops 400
   end
 end
