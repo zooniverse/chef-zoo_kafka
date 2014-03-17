ac5ec67d4664bcca0eb3b7145feffcc64f0297d4
Edward Paget <ed.paget@gmail.com>
2014-03-17 15:28:21 -0500

    Add zookeeper ports to kafka

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,7 +1,7 @@
 override['java']['jdk_version'] = 7
 
 override['kafka']['number_of_brokers'] = 3
-override['kafka']['hostname'] = "kafka1.zooniverse.org"
+override['kafka']['ad_hostname'] = "kafka1.zooniverse.org"
 override['kafka']['zookeeper_hosts'] = [
   "zk1.zooniverse.org:2181",
   "zk2.zooniverse.org:2181",
